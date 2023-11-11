ce3584d2d895a76f2de8e5a761311a0c612abd4d
S <sakil.m880@gmail.com>
2023-11-11 14:20:50 -0700

    fixed auth

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,72 +4,87 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const AWS = require('aws-sdk');
+const { v4: uuidv4 } = require('uuid');
 
 // Configure AWS
 AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_KEY,
-  region: process.env.AWS_REGION
+accessKeyId: process.env.AWS_ACCESS_KEY,
+secretAccessKey: process.env.AWS_SECRET_KEY,
+region: process.env.AWS_REGION
 });
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 router.post('/register', async (req, res) => {
-    const { name, email, password } = req.body;
+    const { name, email, location, role, password } = req.body;
+    
+    // Validate user input here (left out for brevity)
     
     // Hash password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
     
-    // Create user object
+    // Create UID for the new user
+    const uid = uuidv4();
+
+    // Create user object for DynamoDB
     const user = {
-      TableName: 'Users',
-      Item: {
-        email: email,
-        name: name,
-        password: hashedPassword
-      }
+    TableName: 'Users',
+    Item: {
+        UID: uid,
+        Name: name,
+        Email: email,
+        Location: location,
+        Role: role,
+        Password: hashedPassword
+    }
     };
-  
+
     // Save user to DynamoDB
     dynamoDb.put(user, (err, data) => {
-      if (err) {
+    if (err) {
         res.status(400).send(err);
-      } else {
-        res.status(201).send({ message: 'User created successfully' });
-      }
+    } else {
+        res.status(201).send({ UID: uid, message: 'User created successfully' });
+    }
     });
-  });
-  
+});
 
-  router.post('/login', async (req, res) => {
+
+
+router.post('/login', async (req, res) => {
     const { email, password } = req.body;
-    
-    // Find user in DynamoDB
+
+    // Validate user input here (left out for brevity)
+
+    // Find user in DynamoDB by email
     const params = {
-      TableName: 'Users',
-      Key: {
-        email: email
-      }
+    TableName: 'Users',
+    IndexName: 'EmailIndex', // This assumes you've set up a GSI on email
+    KeyConditionExpression: 'Email = :email',
+    ExpressionAttributeValues: {
+        ':email': email
+    }
     };
-  
-    dynamoDb.get(params, async (err, data) => {
-      if (err) {
+
+    dynamoDb.query(params, async (err, data) => {
+    if (err) {
         res.status(500).send(err);
-      } else {
-        if (data.Item) {
-          // Check password
-          const validPass = await bcrypt.compare(password, data.Item.password);
-          if (!validPass) return res.status(400).send('Invalid password');
-          
-          // Create and assign token
-          const token = jwt.sign({ email: data.Item.email }, process.env.TOKEN_SECRET);
-          res.header('auth-token', token).send(token);
+    } else {
+        if (data.Items.length > 0) {
+        const user = data.Items[0];
+        // Check password
+        const validPass = await bcrypt.compare(password, user.Password);
+        if (!validPass) return res.status(400).send('Invalid password');
+        
+        // Create and assign token
+        const token = jwt.sign({ UID: user.UID }, process.env.TOKEN_SECRET);
+        res.header('auth-token', token).send(token);
         } else {
-          res.status(400).send('Email not found');
+        res.status(400).send('Email not found');
         }
-      }
+    }
     });
-  });  
+});
 
 module.exports = router;
